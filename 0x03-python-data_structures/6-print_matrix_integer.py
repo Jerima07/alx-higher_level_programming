f4e70e738fc5a0735148b681c4719272fcae7920
MUMIN <abdulmuminjerima@gmail.com>
2022-09-27 06:44:18 -0700

    matrix of integers

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -4,10 +4,10 @@ def print_matrix_integer(matrix=[[]]):
         print()
     else:
         for i in range(len(matrix)):
-            for j in range(len(matrix([i])):
-                    if j != len(matrix[i]) - 1:
-                       endspace = ' '
-                    else:
-                        endspace = ''
-                    print("{:d}".format(matrix[i][j]), end=endspace)
+            for j in range(len(matrix[i])):
+                if j != len(matrix[i]) - 1:
+                    endspace = ' '
+                else:
+                    endspace = ''
+                print("{:d}".format(matrix[i][j]), end=endspace)
             print()
